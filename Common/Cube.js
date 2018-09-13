881d5413cfa653595bac5497110451ec50cb250a
chrisTorrecampo <christorrecampo@gmail.com>
2018-09-12 20:36:57 -0700

    Update Cube.js

--- Common/Cube.js
+++ Common/Cube.js
@@ -20,8 +20,8 @@ function Cube( vertexShaderId, fragmentShaderId ) {
         values : new Float32Array([
             0.0, 0.0, 0.0,  //Vertex 0
             1.0, 0.0, 0.0,  //Vertex 1
-            1.0, 1.0, 0.0,  //Vertex 2
-            0.0, 1.0, 0.0,  //Vertex 3           
+            1.0, 1.0, 1.0,  //Vertex 2
+            0.0, 1.0, 1.0  //Vertex 3           
         ]),
         numComponents : 3
     };
